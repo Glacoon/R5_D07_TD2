544312f88ab6d57358349c667bb4bd59bb887e54
CELLE Guillian <guillian.celle@gmail.com>
2023-10-06 09:43:44 +0200

    initialisation

---
+++ Facteur_Premier/src/main/java/Facteurs_premiers/Main.java
@@ -0,0 +1,10 @@
+package Facteurs_premiers;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello World");
+    }
+}
+
+
---
+++ Facteur_Premier2/src/main/java/FacteursPremiers/Main.java
@@ -0,0 +1,10 @@
+package FacteursPremiers;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello World");
+    }
+}
+
+
