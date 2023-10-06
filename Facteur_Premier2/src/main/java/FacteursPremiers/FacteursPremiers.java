a99a3c2944ca44e051b8dd7d8bf39d7f4bc39c23
CELLE Guillian <guillian.celle@gmail.com>
2023-10-06 10:18:25 +0200

    Test1

--- Facteur_Premier2/src/main/java/FacteursPremiers/FacteursPremiers.java
+++ Facteur_Premier2/src/main/java/FacteursPremiers/FacteursPremiers.java
@@ -1,5 +1,17 @@
 package FacteursPremiers;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
 public class FacteursPremiers {
 
+    public Set<Integer> generate(int i) {
+        Set<Integer> set = new HashSet<>();
+
+        return set;
+
+    }
 }
--- Facteur_Premier2/src/test/java/FacteursPremiers/FacteursPremiersTest.java
+++ Facteur_Premier2/src/test/java/FacteursPremiers/FacteursPremiersTest.java
@@ -2,12 +2,21 @@ package FacteursPremiers;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FacteursPremiersTest {
 
-    @Test
-    public void test1_return_test_vide(){
+    FacteursPremiers facteursPremiers = new FacteursPremiers();
 
+    @Test
+    public void test1_return_test_vide() {
+
+        //When
+        Set<Integer> set = facteursPremiers.generate(1);
+
+        //Then
+        assertEquals(set, Set.of());
     }
 }
